54da55b5bdd2accf25ca196752ddb2c3716ec4d6
FranBisquerra <francesc.bisquerra@gmail.com>
2016-02-05 12:05:03 +0100

    cambio fichero de properties, tiene que estar asi

--- practica-2/DuckBoard/src/java/database/www/DBProperties.java
+++ practica-2/DuckBoard/src/java/database/www/DBProperties.java
@@ -2,7 +2,7 @@ package database.www;
 
 public class DBProperties {
 
-  public static final String HOST = "192.168.99.100";
+  public static final String HOST = "127.0.0.1";
   public static final String PORT = "3306";
   public static final String DB   = "www";
   public static final String USER = "root";
