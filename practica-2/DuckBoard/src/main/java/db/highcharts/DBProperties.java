04557e250424477ceba2e38c84567ec8c923f62a
Lluc Martorell <superpesi@gmail.com>
2016-02-08 00:27:50 +0100

    default: DBProperties fixed

--- practica-2/DuckBoard/src/main/java/db/highcharts/DBProperties.java
+++ practica-2/DuckBoard/src/main/java/db/highcharts/DBProperties.java
@@ -2,7 +2,7 @@ package db.highcharts;
 
 public class DBProperties {
 
-  public static final String HOST = "192.168.99.100";
+  public static final String HOST = "localhost";
   public static final String PORT = "3306";
   public static final String DB   = "highcharts";
   public static final String USER = "root";
