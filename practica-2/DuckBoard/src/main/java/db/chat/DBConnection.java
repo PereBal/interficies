5b5110fab09226839700535e27a458bca742cab4
Lluc Martorell <superpesi@gmail.com>
2016-02-08 01:18:43 +0100

    default: Hotfix

---
+++ practica-2/DuckBoard/src/main/java/db/chat/DBConnection.java
@@ -0,0 +1,50 @@
+package db.chat;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
+public class DBConnection implements java.lang.AutoCloseable {
+
+  private MongoClient client;
+  private MongoClientURI con;
+
+  public DBConnection() {
+    con = null;
+    client = null;
+  }
+
+  public void open() {
+    try {
+      if (client == null) {
+        client = new MongoClient(db.chat.DBProperties.HOST, db.chat.DBProperties.PORT);
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  @Override
+  public void close() {
+    try {
+      client.close();
+      client = null;
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public MongoClient getConnection() throws NullPointerException {
+    if (client == null) {
+      throw new NullPointerException("Connection not opened");
+    }
+    return client;
+  }
+
+  public MongoDatabase getDatabase() throws NullPointerException {
+    if (client == null) {
+      throw new NullPointerException("Connection not opened");
+    }
+    return client.getDatabase(DBProperties.DB);
+  }
+}
