ac78b6224d169fa6d6117af680076711793bacdb
bcrespi <tomeu_crespi25@hotmail.com>
2016-02-08 01:27:56 +0100

    fixed sample

--- practica-2/DuckBoard/src/main/java/db/chat/Sample.java
+++ practica-2/DuckBoard/src/main/java/db/chat/Sample.java
@@ -3,7 +3,7 @@ package db.chat;
 public class Sample {
 
   public static final String HOST = "localhost";
-  public static final String PORT = "27017";
+  public static final int PORT    = 27017;
   public static final String DB   = "chat";
   public static final String COLL = "chats";
   public static final String OPTS = "";
